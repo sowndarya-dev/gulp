6dc5c29f3452548e4e8dadb38ba4d0eba3abdf08
sowndarya <skurri@blusapphire.net>
2020-06-18 20:50:44 +0530

    Support for babel and browserify

---
+++ gulpfile.babel.js
@@ -0,0 +1,59 @@
+//Initialize Modules
+const { src,dest,watch,series,parallel}=require('gulp');
+const autoprefixer= require('autoprefixer');
+const cssnano= require('cssnano');
+const concat= require('gulp-concat');
+const postcss= require('gulp-postcss');
+const replace= require('gulp-replace');
+const sass= require('gulp-sass');
+const sourcemaps= require('gulp-sourcemaps');
+const uglify= require('gulp-uglify');
+const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
+//File path variables
+const files={
+    scssPath:'app/scss/**/*.scss',
+    jsPath:'app/js/**/*.js'
+}
+
+//Sass tasks
+function sassTask(){
+    return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(),cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('dist'))
+}
+
+//JS tasks
+function jsTask(){
+    return src(files.jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(browserify({ insertGlobals : true,}))
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+   // .pipe(uglify())
+    .pipe(dest('dist'))
+}
+
+//Cachebursting
+const cbstring=new Date().getTime();
+function cacheBustTask(){
+    return src(['index.html'])
+    .pipe(replace(/cb=\d+/g,'cb='+ cbstring)) 
+    .pipe(dest('.'))
+}
+
+
+//watch tasks
+function watchTask(){
+    watch([files.scssPath,files.jsPath],
+        parallel(sassTask,jsTask))
+}
+
+//Default task
+exports.default=series(parallel(sassTask,jsTask),cacheBustTask,watchTask)
